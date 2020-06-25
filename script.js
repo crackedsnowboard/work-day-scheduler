04abc09c943a8858e315295c25d46d52cc6f7cdc
Prince of Kochi <mathenjs@gmail.com>
2020-06-24 18:24:11 -0700

    wrote current date time and schedule

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+// #1 Display current date
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+})
+
+// #2 figure out Present timeblocks in html via js
+$("#container").html("hello");
+$("#container")
+
+
+// #3 figure out how to shade time blocks based on past, present future. past grey, current hour red, future green
+
+//  #4 - click time block can enter text event
+
+//  #5 save the event for the that timeblock. Aylay local session storage. // 04 - code drills. 04-signin-localstorage review local storage 
+
+//  #6 when page refresh the saved events persist. local will persist. 
+
