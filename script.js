7cf86e0f2036dad69ea43fda635e1ed93b031f5f
Prince of Kochi <mathenjs@gmail.com>
2020-06-25 15:44:07 -0700

    Stored one item to local storage

--- script.js
+++ script.js
@@ -5,31 +5,32 @@ $(document).ready(function () {
 })
 
 // #2 figure out Present timeblocks in html via js
-// $("#container").html("hello");
-// $("#container")
+
 
 
 // #3 figure out how to shade time blocks based on past, present future. past grey, current hour red, future green
+
+function highlightTime () {
 var hour = moment().format("h");
 console.log(hour);
+
+}
+
 // function; loop - set green; grey
 
 
 //  #4 - click time block can enter text event
-
-
-
 //  #5 save the event for the that timeblock. Aylay local session storage. // 04 - code drills. 04-signin-localstorage review local storage 
-$(".button").on("click", function () {
-    var inputInfo = event.target;
-    console.log(inputInfo)
+$(".buttonRead").on("click", function () {
+    console.log(this);
+    var dataAttribInfo = $(this).attr("data-button")
+    console.log(dataAttribInfo);
     var inputUser = $(".hourInput").val()
     console.log(inputUser)
+    localStorage.setItem("data", JSON.stringify(dataAttribInfo))
     localStorage.setItem("plans", JSON.stringify(inputUser));
-
 }
 );
-
 
 // var lastUser = JSON.parse(localStorage.getItem("user"));
 
